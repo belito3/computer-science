9db8ced6bb0fb8ed32f60209d68949eb084b3716
bao <bao.buivan3@gmail.com>
2019-12-03 08:54:31 +0700

    add solution 2 s3

--- algo/3/s1.py
+++ algo/3/s1.py
@@ -10,6 +10,17 @@ class Solution:
             rs = max(rs, right - left + 1)
         return rs
 
+    def lengthOfLongestSubstring2(self, s: str) -> int:
+        rs, left = 0, 0
+        lookup = [None] * 128
+        # ord(c): return ASCII value
+        for right in range(len(s)):
+            if lookup[ord(s[right])] is not None:
+                left = max(left, lookup[ord(s[right])] + 1)
+            lookup[ord(s[right])] = right
+            rs = max(rs, right - left + 1)
+        return rs
+
 
 
 s = Solution()
@@ -17,3 +28,4 @@ s = Solution()
 #
 
 print(s.lengthOfLongestSubstring("dvxdfedabcx"))
+print(s.lengthOfLongestSubstring2("dvxdfedabcx"))
