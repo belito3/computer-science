63e1f6a68fa5c3d759732860032a3414e4b6c3c1
bao <bao.buivan3@gmail.com>
2019-12-25 14:07:08 +0700

    add solution recursive

--- algo/linked_list/206/s1.py
+++ algo/linked_list/206/s1.py
@@ -4,6 +4,7 @@ class ListNode:
         self.next = None
 
 class Solution:
+    # solution 1: interative
     def reserveList(self, head: ListNode) -> ListNode:
         prev = None
         curr = head    
@@ -14,6 +15,16 @@ class Solution:
             prev = curr 
             curr = tmp
         return prev
+
+    # solution 2: recursive
+    def reserveList2(self, head: ListNode) -> ListNode:
+        if(head is None or head.next is None): 
+            return head
+        
+        p = self.reserveList2(head.next)
+        head.next.next = head
+        head.next = None
+        return p
 
 def create_linked_list(l):
     dummy = current = ListNode(None)
@@ -34,6 +45,6 @@ def print_linked_list(head):
 
 s = Solution()
 # [1]
-l = create_linked_list([])
+l = create_linked_list([1, 2, 2, 3, 4, 5])
 
-print_linked_list(s.reserveList(l))
+print_linked_list(s.reserveList2(l))
