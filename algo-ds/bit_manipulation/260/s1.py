df5ff0d75f530b6cf7c8336f8b23a9cec0c6956f
bao <bao.buivan3@gmail.com>
2020-02-17 14:59:15 +0700

    add solution 260: bit

--- algo-ds/bit_manipulation/260/s1.py
+++ algo-ds/bit_manipulation/260/s1.py
@@ -2,6 +2,24 @@ from typing import List
 
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
+        # Find diff
+        diff = 0
+        for n in nums:
+            diff ^= n
+
+        diff &= -diff  # ~(diff - 1)
+
+        num1 = 0
+        num2 = 0
+        for n in nums:
+            if (n & diff) == 0:
+                num1 ^= n
+            else:
+                num2 ^= n
+
+        return [num1, num2]
+
+    def singleNumber2(self, nums: List[int]) -> List[int]:
         s = set()
 
         for n in nums:
@@ -12,5 +30,4 @@ class Solution:
         return list(s)
 
 s = Solution()
-
-print(s.singleNumber([1, 2, 3, 3])
+print(s.singleNumber([2,3,4,4,5,5]))
