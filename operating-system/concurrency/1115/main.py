68bb66fc21081b42e150164a59d02fa565a66730
bao <bao.buivan3@gmail.com>
2020-09-04 08:52:47 +0700

    update 1115

--- operating-system/concurrency/1115/main.py
+++ operating-system/concurrency/1115/main.py
@@ -1,3 +1,42 @@
+# import threading
+
+# class FooBar:
+#     def __init__(self, n):
+#         self.n = n
+
+#     def foo(self, printFoo: 'Callable[[], None]') -> None:
+#         for i in range(self.n):
+#             # printFoo() outputs "foo". Do not change or remove this line.
+#             printFoo()
+
+#     def bar(self, printBar: 'Callable[[], None]') -> None:
+#         for i in range(self.n):
+#             # printBar() outputs "bar". Do not change or remove this line.
+#             printBar()
+
+# n = 5
+
+# foo_bar = FooBar(n)
+
+
+# def print_foo():
+#     print("foo")
+
+
+# def print_bar():
+#     print("bar")
+
+
+# t1 = threading.Thread(target=foo_bar.foo, args=(print_foo,))
+# t2 = threading.Thread(target=foo_bar.bar, args=(print_bar,))
+
+# t1.start()
+# t2.start()
+
+# t1.join()
+# t2.join()
+
+
 import threading
 
 # s1: only event
