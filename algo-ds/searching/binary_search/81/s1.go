2055881f2ccb030375f94772b32052cb156e76d6
bao <bao.buivan3@gmail.com>
2020-10-07 17:22:17 +0700

    solution 81

--- algo-ds/searching/binary_search/81/s1.go
+++ algo-ds/searching/binary_search/81/s1.go
@@ -3,49 +3,69 @@ package main
 import "fmt"
 
 func main() {
-	nums := []int{2, 5, 6, 0, 0, 1, 2}
-	target := 7
+	//nums := []int{2, 5, 6, 0, 0, 1, 2}
+	//target := 7
+    //nums := []int{2, 2, 2, 0, 2, 2}
+    //target := 3
+    nums := []int{2, 2, 2, 2, 2, 2}
+    target := 2
+
 	fmt.Printf("target = %v, nums = %v, rs = %v \n", target, nums, search(nums, target))
 }
 
 func search(nums []int, target int) bool {
-	if len(nums) == 0 {
-		return false
-	}
+    // time: worst O(N), best O(logN)
 
-	l, r := 0, len(nums)-1
-	// find rotated point
-	for l < r {
-		m := (l + r) / 2
-		if nums[m] > nums[r] {
-			l = m + 1
-		} else {
-			r = m
-		}
-	}
+    size := len(nums)
+    if size == 0 {
+        return false
+    }
 
-	// l is rotated point
-	if nums[l] <= target || len(nums) == 1 {
-		// search in (l, len(nums) - 1)
-		if bs(nums, l, len(nums)-1, target) {
-			return true
-		}
-	}
+    start, end := 0, size - 1
 
-	// search in (0, l - 1)
-	return bs(nums, 0, l-1, target)
+    for start <= end {
+        mid := (start + end) / 2
+        if nums[mid] == target {
+            return true
+        }
+
+        // if nums[start] == nums[mid], nums[mid] might belong to both F and S
+        // iterators to search target 
+        if !isBinarySearchHelpfull(nums, start, nums[mid]) {
+            start++
+            continue
+        }
+
+        // which array does mid belong to
+        pivotArray := existsInFirst(nums, start, nums[mid])
+        // which array does target belong to
+        targetArray := existsInFirst(nums, start, target)
+
+        if pivotArray != targetArray {
+            // mid and target lies in different array
+            if pivotArray {
+                start = mid + 1
+            } else {
+                end = mid - 1
+            }
+        } else {
+            // mid and target lies in same array
+            if nums[mid] < target {
+                start = mid + 1
+            } else {
+                end = mid - 1
+            }
+        }
+
+    }
+    return false
 }
 
-func bs(nums []int, l, r, target int) bool {
-	for l <= r {
-		m := (l + r) / 2
-		if nums[m] == target {
-			return true
-		} else if nums[m] < target {
-			l = m + 1
-		} else {
-			r = m - 1
-		}
-	}
-	return false
+func existsInFirst(nums []int, start, element int) bool {
+    return element >= nums[start]
+}
+
+// neu nums[start] == nums[mid] -> nums[mid] co the nam trong array first or array second
+func isBinarySearchHelpfull(nums []int, start, element int) bool {
+    return nums[start] != element
 }
