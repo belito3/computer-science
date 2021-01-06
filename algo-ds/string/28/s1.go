d4c7d4e0a493fcfcb1192e05f939f1a45f4e2009
baobui <bao.buivan3@gmail.com>
2021-01-06 23:35:55 +0700

    add annotation

--- algo-ds/string/28/s1.go
+++ algo-ds/string/28/s1.go
@@ -3,31 +3,41 @@ package main
 import "fmt"
 
 func main() {
-    test := []string{"hello", "ll", "aaaaaa", "bba", "", "a", "a", "a", ""}
+    test := []string{"hello", "ll", "aaaaaa", "bba", "", "a", "a", "a", "mississippi", "issip"}
 
     for i := 0; i < len(test); i += 2 {
-        fmt.Printf("s1=%v  s2=%v  i=%v\n", test[i], test[i+1], strStr(test[i], test[i+1]))
+        fmt.Printf("s1=%v  s2=%v  i=%v\n", test[i], test[i+1], strStr1(test[i], test[i+1]))
     }
 }
 
 func strStr(haystack string, needle string) int {
-    if needle == "" {
-        return 0
-    }
-
-    for i, j := 0, 0; j < len(haystack); {
-        //fmt.Println("j = ", j, string(haystack[j]))
-        //fmt.Println("i = ", i, string(needle[i])) 
-        if needle[i] == haystack[j] {
-            if i == len(needle) - 1 {
-                return j - i
-            }
-            i += 1
-        } else {
-            i = 0
-        }
-        j += 1
-    }
-
-    return -1
+	return 0
 }
+
+func strStr1(haystack string, needle string) int {
+	// S1: Bureforce - Native Search
+	// Time: O(n) - O(m*n)
+	// Space: O(1)
+	if needle == "" {
+		return 0
+	}
+	m := len(haystack)
+	n := len(needle)
+
+	for i := 0; i < m - n + 1; i++ {
+		j := 0
+		for ; j < n; j++ {
+			if haystack[i+j] != needle[j] {
+				break
+			}
+		}
+		// trick j++
+		if j == n {
+			return i
+		}
+	}
+
+	return -1
+}
+
+
