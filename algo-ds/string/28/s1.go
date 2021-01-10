a690724d5d5ef120208092c53d3738e7b8a918c6
baobui <bao.buivan3@gmail.com>
2021-01-10 22:45:38 +0700

    add 28

--- algo-ds/string/28/s1.go
+++ algo-ds/string/28/s1.go
@@ -6,13 +6,67 @@ func main() {
     test := []string{"hello", "ll", "aaaaaa", "bba", "", "a", "a", "a", "mississippi", "issip"}
 
     for i := 0; i < len(test); i += 2 {
-        fmt.Printf("s1=%v  s2=%v  i=%v\n", test[i], test[i+1], strStr1(test[i], test[i+1]))
+        fmt.Printf("s1=%v  s2=%v  i=%v\n", test[i], test[i+1], strStr(test[i], test[i+1]))
     }
+    fmt.Println(computeLPS("AAACAAAA"))
 }
 
 func strStr(haystack string, needle string) int {
-	return 0
+	m := len(haystack)
+	n := len(needle)
+
+	if n == 0 {
+		return 0
+	}
+
+	lps := computeLPS(needle)
+
+	for i, j := 0, 0; i < m; {
+		if needle[j] == haystack[i] {
+			i += 1
+			j += 1
+		}
+		if j == n {
+			return i - j
+		}
+
+		if i < m && haystack[i] != needle[j] {
+			if j == 0 {
+				i += 1
+			} else {
+				j = lps[j-1]
+			}
+		}
+	}
+
+	return -1
 }
+
+func computeLPS(needle string) []int {
+	M := len(needle)
+	lps := make([]int, M)
+
+	lps[0] = 0
+	i := 1
+	l := 0
+	for i < M {
+		if needle[i] == needle[l] {
+			l += 1
+			lps[i] = l
+			i += 1
+		} else {
+			if l != 0 {
+				l = lps[l-1] // ???/
+			} else {
+				lps[i] = 0
+				i += 1
+			}
+
+		}
+	}
+	return lps
+}
+
 
 func strStr1(haystack string, needle string) int {
 	// S1: Bureforce - Native Search
