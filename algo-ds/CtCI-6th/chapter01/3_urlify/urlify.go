5df2ea3aaa0b90a5520a420c070288000bd53a1a
baobui <bao.buivan3@gmail.com>
2021-06-07 16:38:40 +0700

    add urlify solution

--- algo-ds/CtCI-6th/chapter01/3_urlify/urlify.go
+++ algo-ds/CtCI-6th/chapter01/3_urlify/urlify.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
 
 func main() {
 	// Input: "Mr John Smith    ", 13
@@ -13,8 +17,55 @@ func main() {
 }
 
 
-func replaceSpace(str string, length int) string {
-	
+func replaceSpace(str string, trueLength int) string {
+	arr := strings.Split(str, "")	
+	index := len(str) - 1
+
+	for i := trueLength - 1; i >= 0; i-- {
+		if arr[i] == " " {
+			arr[index] = "0"	
+			arr[index-1] = "2"
+			arr[index-2] = "%"
+			index -= 3
+		} else {
+			arr[index] = arr[i]
+			index--	
+		}
+	}
+	return strings.Join(arr, "")
+}
+
+func replaceSpace2(str string, trueLength int) string {
+	// Edit string starting from the end
+	// Author: !!!bao baby !!!
+	right := len(str)
+	arr := strings.Split(str, "")
+	for i := trueLength - 1; i >= 0; i-- {
+		if arr[i] == " " {
+			fmt.Println("i ", i)
+			// move str[i+1:trueLength] to str[i+1+right-trueLength:right]
+			start := i+1+right-trueLength
+			fmt.Println("before ", arr)
+			moveList(arr, start, right, trueLength-1)
+			fmt.Println("after  ", arr)
+			// arr[start:right] = arr[i+1:trueLength] 	
+			arr[start-3] = "%"
+			arr[start-2] = "2"
+			arr[start-1] = "0"
+			right = start - 3
+			trueLength = i 
+		}		
+	} 	
+	return strings.Join(arr, "")
+}
+
+func moveList(arr []string, start, end, index int) {
+	fmt.Println(start, end, index)
+	for i := end-1; i >= start; i-- {
+		//fmt.Println("i: ", i)
+		arr[i] = arr[index]	
+		index -= 1
+	}	
 }
 
 func replaceSpace1(str string, length int) string {
