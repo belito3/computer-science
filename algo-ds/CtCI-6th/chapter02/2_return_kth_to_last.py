458ac22cdb0309e2a7d7cc81d0f37233d3a2604d
baobui <bao.buivan3@gmail.com>
2021-06-27 10:33:00 +0700

    add recursive solution

--- algo-ds/CtCI-6th/chapter02/2_return_kth_to_last.py
+++ algo-ds/CtCI-6th/chapter02/2_return_kth_to_last.py
@@ -1,6 +1,24 @@
 from linked_list import ListNode, createLinkedList, printLinkedList
 
+def printKthToLast(node: ListNode, k: int) -> ListNode:
+    # Recursive
+    # Time: O(n)
+    # Space: O(n)
+    if node == None:
+        return 0
+
+    index = printKthToLast(node.next, k) + 1
+    if index == k:
+        print(k, "th to last node is " + str(node.val))
+
+    return index
+
+    
+
 def return_kth_to_last(node: ListNode, k: int) -> ListNode:
+    # Two Point
+    # Time: O(N)
+    # Space: O(1)
     node1 = node2 = node
 
     for i in range(k):
@@ -22,5 +40,7 @@ if __name__ == "__main__":
             node = createLinkedList(arr[i])
             target = return_kth_to_last(node, j)
             print("rs :", target.val)
+
+    printKthToLast(createLinkedList(arr[0]), 3)
 
 
