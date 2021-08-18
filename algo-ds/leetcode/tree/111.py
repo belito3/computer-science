5b1b4eb9490f3404790fad02d0de06af7d1912b0
baobui <bao.buivan3@gmail.com>
2021-08-18 16:09:10 +0700

    add 111 bfs solution

--- algo-ds/leetcode/tree/111.py
+++ algo-ds/leetcode/tree/111.py
@@ -1,4 +1,5 @@
 from binary_tree import TreeNode, printTree
+from collections import deque
 
 
 def minDepth(root: Optional[TreeNode]) -> int:
@@ -11,4 +12,29 @@ def minDepth(root: Optional[TreeNode]) -> int:
     if (root.left is None) and (root.right is not None):
         return 1 + minDepth(root.right)
     return 1 + min(minDepth(root.left), minDepth(root.right))
+
+
+def minDepthBFS(root: Optional[TreeNode]) -> int:
+    if root is None:
+        return 0
+
+    q = deque()
+    q.append(root)
+    layer = 1
+
+    while q:
+        for _ in range(len(q)):
+            n = q.popleft()
+            if node.left is None and node.right is None:
+                return layer
+
+            if node.left is not None:
+                q.append(node.left)
+
+            if node.right is not None:
+                q.append(node.right)
+        layer += 1
+    return 0
+
+
 
