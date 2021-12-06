30be9ed932c85702c09d8a1d7a22c991cde92622
baobui <bao.buivan3@gmail.com>
2021-12-06 17:55:18 +0700

    add 9 parents

--- algo-ds/CtCI-6th/chapter08/9_parens.py
+++ algo-ds/CtCI-6th/chapter08/9_parens.py
@@ -1,13 +1,44 @@
 from typing import List
 
-def get_parens(num: int) ->  List[int]:
-    rs = make_parens(num)
-    return [r[0] for r in rs]
+def get_parens(num: int) -> List[int]:
+    pass
 
-def make_parens(num):
+
+def make_parens(num: int) -> List[int]:
+    if num == 0:
+        return [("", 0, 0)]
+
+    rs = make_parens(num-1)
+    for
+
+
+def get_parens2(num: int) ->  List[int]:
+    rs = make_parens(num)
+    return list(rs)
+
+
+def make_parens2(num: int):
+    # DP with hash table
     # Time: O(2^n)
     # Space: O(2^n)
-    rs = [("", 0, 0)] 
+    if num <= 1:
+        return {"()"}
+
+    rs = make_parens(num-1)
+    rs_temp = set()
+    for p in rs:
+        for i in range(len(p) + 1):
+            str_ = str(p[0:i] + "()" + p[i:])
+            rs_temp.add(str_)
+    return rs_temp
+
+
+
+def make_parens1(num):
+    # S1: Brute force
+    # Time: O(2^n/2 -> 2^n)  -> Only need select position for open parenthesis "("
+    # Space: O(2^n/2 -> 2^n)
+    rs = [("", 0, 0)]
     if num <= 0:
          # prefix, open parenthesis, close parenthesis
         return rs
@@ -28,8 +59,6 @@ def make_parens(num):
                 if close_ < num:
                     rs_temp.append((p + ")", open_, close_+1))
         rs = rs_temp
-        #print(f"rs_temp = {rs_temp}")
-
     return rs
 
 if __name__ == "__main__":
