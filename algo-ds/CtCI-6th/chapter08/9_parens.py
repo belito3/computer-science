141a096358b127bd464ca91092ceb5de37fbcdd8
baobui <bao.buivan3@gmail.com>
2021-12-07 16:34:14 +0700

    add 9 parens

--- algo-ds/CtCI-6th/chapter08/9_parens.py
+++ algo-ds/CtCI-6th/chapter08/9_parens.py
@@ -1,15 +1,50 @@
 from typing import List
 
 def get_parens(num: int) -> List[int]:
-    pass
+    arr = []
+    str_ = ["" for i in range(num*2)]
+    make_parens(arr, str_, num, num, 0)
+    return arr
+
+def make_parens(arr: List[str], str_, leftRe, rightRe, index):
+    if leftRe < 0 or leftRe > rightRe:
+        return
+
+    if leftRe == 0 and rightRe == 0:
+        arr.append("".join(str_))
+    else:
+        str_[index] = "("
+        make_parens(arr, str_, leftRe-1, rightRe, index+1)
+
+        str_[index] = ")"
+        make_parens(arr, str_, leftRe, rightRe-1, index+1)
 
 
-def make_parens(num: int) -> List[int]:
+def get_parens3(num: int) -> List[int]:
+    rs  = make_parens(num, num)
+    return [r[0] for r in rs]
+
+
+def make_parens3(num: int, limit: int) -> List[int]:
+    # DP + count num open and close parenthesis
     if num == 0:
         return [("", 0, 0)]
 
-    rs = make_parens(num-1)
-    for
+    rs = make_parens(num-1, limit)
+    while rs[0][1] < num or rs[0][2] < num:
+        rs_temp = []
+        for e in rs:
+            p = e[0]
+            open_ = e[1]
+            close_ = e[2]
+            if (open_ == close_):
+                rs_temp.append((p+"(", open_+1, close_))
+            elif open_ <= limit and open_ > close_:
+                if open_ < limit:
+                    rs_temp.append((p+"(", open_+1, close_))
+                rs_temp.append((p+")", open_, close_+1))
+        rs = rs_temp
+    return rs_temp
 
 
 def get_parens2(num: int) ->  List[int]:
@@ -62,7 +97,7 @@ def make_parens1(num):
     return rs
 
 if __name__ == "__main__":
-    for i in range(4):
+    for i in range(5):
         rs = get_parens(i)
         print(f"i={i}: {rs}")
 
