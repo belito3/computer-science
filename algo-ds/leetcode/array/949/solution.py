5436b2d03acfaee1364b2b9a7e4f2ce07d79c0dd
baobui <bao.buivan3@gmail.com>
2022-03-10 22:24:03 +0700

    add 949 Largest Time for given digits

--- algo-ds/leetcode/array/949/solution.py
+++ algo-ds/leetcode/array/949/solution.py
@@ -2,17 +2,44 @@ from typing import List
 
 class Solution:
     def largestTimeFromDigits(self, arr: List[int]) -> str:
-        pass
+        times = getPermutation2(arr)
+        print(times)
+        times = sorted(times)
 
+        for i in range(len(times)-1, -1, -1):
+            if checkTimeValid(times[i]):
+                return times[i][0:2] + ":" + times[i][2:4]
+
+        return ""
+
+def checkTimeValid(time: str) -> bool:
+    """
+    H1H2:M1M2
+
+    H1:  0 <= H1 <= 2  
+    if H1 == 2 -> 0 <= H2 <= 3
+    M1: 0 <= M1 <= 5
+    """
+    print(f"time: {time}")
+    h1, h2, m1, m2 = int(time[0]), int(time[1]), int(time[2]), int(time[3])
+    if h1 > 2:
+        return False
+
+    if h1 == 2 and h2 > 3:
+        return False
+
+    if m1 > 5: 
+        return False
+    return True
 
 def getPermutation2(arr):
     rs = []
-    num = 0
+    num = ""
     getPermutationDistinct(arr, num, rs)
     return rs
 
 
-def getPermutationDistinct(arr: List[int], num: int, rs: List[int]):
+def getPermutationDistinct(arr: List[int], num: str, rs: List[int]):
     if len(arr) == 0:
         rs.append(num)
 
@@ -20,7 +47,7 @@ def getPermutationDistinct(arr: List[int], num: int, rs: List[int]):
 
     for i in range(len(arr)):
         if table[arr[i]] is False:
-            num2 = num * 10 + arr[i]
+            num2 = num + str(arr[i])
             new_arr = arr[:i] + arr[i+1:]
             getPermutationDistinct(new_arr, num2, rs)
             table[arr[i]] = True
@@ -54,5 +81,9 @@ if __name__ == "__main__":
     rs = getPermutation2(arr)
     print(rs)
 
+    arr = [[1,2,3,4], [0,0,0,0]]
+    for a in arr:
+        s = Solution()
+        print(s.largestTimeFromDigits(a))
 
 
